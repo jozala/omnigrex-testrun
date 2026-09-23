1557e85330579f7a68bfb362501ef3ad8290eeb4
Omnigrex Developer <developer@omnigrex.invalid>
2026-09-23 08:15:23 +0000

    Prompt for user name and greet personally
    Omnigrex-Operation-ID: eaa7b6e1-48db-4729-901e-1fae75a28c7a

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Print("Enter your name: ")
+	var name string
+	_, err := fmt.Scanln(&name)
+	if err != nil {
+		fmt.Println("Hello, World!")
+		return
+	}
+	fmt.Printf("Hello, %s!\n", name)
 }
